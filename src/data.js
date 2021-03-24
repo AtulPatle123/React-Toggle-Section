103f33759bc5accb3f05af641f267d2f5b033c5a
atul <atulpatlexxxgmail.com>
2021-03-24 15:39:55 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    id: "1",
+    order: 3,
+    title: "Full Stack Web Developer",
+    dates: "December 2015 - Present",
+    duties: [
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    ],
+    company: "Google",
+  },
+  {
+    id: "2",
+    order: 2,
+    title: "Front-End Engineer",
+    dates: "May 2015 - December 2015",
+    duties: [
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      ],
+    company: "Facebook",
+  },
+  {
+    id: "3",
+    order: 1,
+    title: "Engineering Intern",
+    dates: "May 2014 - September 2015",
+    duties: [
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+      ],
+    company: "Apple",
+  },
+];
+
